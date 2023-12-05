2c13de51fd83c51145f4adb0282332020b54c1bb
Mark Brown <mark.aleyn@gmail.com>
2023-12-05 11:22:35 +0000

    Initial Commit

---
+++ SocialProfile/SocialProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialProfileApp.swift
+//  SocialProfile
+//
+//  Created by Mark Brown on 05/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
