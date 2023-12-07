d54b430c814fdbc8a14048840017fef70af6a7a0
Mark Brown <mark.aleyn@gmail.com>
2023-12-07 12:21:35 +0000

    I added a new circular view for the profile image and a preview assets for Xcode’s previews. I also added a View extension with a couple of view modifiers to make previews better

--- SocialProfile/CircularImage.swift
+++ SocialProfile/CircularImage.swift
@@ -5,4 +5,24 @@
 //  Created by Mark Brown on 05/12/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct CircularImage: View {
+    let image: Image
+
+    var body: some View {
+        image
+            .resizable()
+            .frame(width: 160.0, height: 160.0)
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.gray, lineWidth: 5.0))
+    }
+}
+
+struct CircularImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularImage(image: Image("mab"))
+            .namedPreview()
+    }
+}
--- SocialProfile/Preview Content/Previews.swift
+++ SocialProfile/Preview Content/Previews.swift
@@ -5,4 +5,25 @@
 //  Created by Mark Brown on 05/12/2023.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+    func previewWithName(_ name: String) -> some View {
+        self
+            .previewLayout(.sizeThatFits)
+            .previewDisplayName(name)
+    }
+
+    func namedPreview() -> some View {
+        let name = String.name(for: type(of: self))
+        return previewWithName(name)
+    }
+}
+
+extension String {
+    static func name<T>(for type: T) -> String {
+        String(reflecting: type)
+            .components(separatedBy: ".")
+            .last ?? ""
+    }
+}
